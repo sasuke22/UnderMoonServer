242936bc489314a2818dc8ad5fa91c51dea7fbd0
江伟进 <jiangweijin8676@yundasys.com>
2020-01-20 17:29:17 +0800

    子评论完成

--- src/main/java/com/qiqiim/constant/SubComment.java
+++ src/main/java/com/qiqiim/constant/SubComment.java
@@ -3,55 +3,103 @@ package com.qiqiim.constant;
 import java.util.Date;
 
 public class SubComment {
-	private int floorId;//层主评论id，对应meetingcomments或者articlecomments的id
-	private int userId;//子评论用户id
-	private String content;//评论内容
-	private Date date;//评论日期
-	private int replyId;//回复谁的id
-	private String replyName;//回复谁的名字
-	private int showName;//是否显示名字
-	public int getFloorId() {
-		return floorId;
-	}
-	public void setFloorId(int floorId) {
-		this.floorId = floorId;
-	}
-	public int getUserId() {
-		return userId;
-	}
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	public int getReplyId() {
-		return replyId;
-	}
-	public void setReplyId(int replyId) {
-		this.replyId = replyId;
-	}
-	public String getReplyName() {
-		return replyName;
-	}
-	public void setReplyName(String replyName) {
-		this.replyName = replyName;
-	}
-	public int getShowName() {
+    private int floorId;//层主评论id，对应meetingcomments或者articlecomments的id
+    private int userId;//子评论用户id
+    private String userName;//子评论用户名字
+    private String content;//评论内容
+    private Date date;//评论日期
+    private int replyId;//回复谁的id
+    private String replyName;//回复谁的名字
+    private boolean showName;//是否显示名字
+    private boolean sheShowName;//回复的评论者是否显示名字
+    private boolean isVip;//年会员
+    private boolean bigVip;//至尊会员
+
+    public int getFloorId() {
+        return floorId;
+    }
+
+    public void setFloorId(int floorId) {
+        this.floorId = floorId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getReplyId() {
+        return replyId;
+    }
+
+    public void setReplyId(int replyId) {
+        this.replyId = replyId;
+    }
+
+    public String getReplyName() {
+        return replyName;
+    }
+
+    public void setReplyName(String replyName) {
+        this.replyName = replyName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+	public boolean isShowName() {
 		return showName;
 	}
-	public void setShowName(int showName) {
+
+	public void setShowName(boolean showName) {
 		this.showName = showName;
 	}
-	
-	
+
+	public boolean isSheShowName() {
+		return sheShowName;
+	}
+
+	public void setSheShowName(boolean sheShowName) {
+		this.sheShowName = sheShowName;
+	}
+
+	public boolean isVip() {
+        return isVip;
+    }
+
+    public void setIsVip(boolean vip) {
+        isVip = vip;
+    }
+
+    public boolean isBigVip() {
+        return bigVip;
+    }
+
+    public void setBigVip(boolean bigVip) {
+        this.bigVip = bigVip;
+    }
 }
--- src/main/java/com/qiqiim/webserver/user/dao/SubCommentsDao.java
+++ src/main/java/com/qiqiim/webserver/user/dao/SubCommentsDao.java
@@ -1,63 +1,57 @@
 package com.qiqiim.webserver.user.dao;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-
-import com.qiqiim.constant.CommentDetail;
 import com.qiqiim.constant.SubComment;
+
+import java.sql.*;
+import java.util.ArrayList;
 
 public class SubCommentsDao {
 	/**
 	 * 对评论进行评论
 	 * @param type 对哪个进行评论，0-邀约;1-反馈;2-社区
-	 * @param comment
-	 * @return
 	 */
 	public static int addCommentToComment(int type,SubComment comment){
 		String sql0 = "use first_mysql_test";
 		String sql1 = null;
 		switch(type){
 			case 0:
-				sql1 = "insert into meeting_sub_comments (floorid,userid,content,date,replyid,showname) values(?,?,?,now(),?,?)";
+				sql1 = "insert into meeting_sub_comments (floorid,userid,content,date,replyid,showname,she_show_name) values(?,?,?,now(),?,?,?)";
 				break;
 			case 1:
-				sql1 = "insert into article_sub_comments (floorid,userid,content,date,replyid,showname) values(?,?,?,now(),?,?)";
+				sql1 = "insert into article_sub_comments (floorid,userid,content,date,replyid,showname,she_show_name) values(?,?,?,now(),?,?,?)";
 				break;
 			case 2:
-				sql1 = "insert into circle_sub_comments (floorid,userid,content,date,replyid,showname) values(?,?,?,now(),?,?)";
+				sql1 = "insert into circle_sub_comments (floorid,userid,content,date,replyid,showname,she_show_name) values(?,?,?,now(),?,?,?)";
 				break;
 		}
 		Connection con = DBPool.getConnection();
+		PreparedStatement ps;
+		ResultSet rs;
+		int result = 0;
 		try {
 			con.setAutoCommit(false);
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		PreparedStatement ps;
-		try {
 			ps = con.prepareStatement(sql0);
 			ps.execute();
+			boolean need = needInsert2ThreeTable(comment,type,con);
+			if (need){
+				result = insert2ThreeTable(comment,type,con);
+				if (result <= 0)
+					return result;
+			}
 			ps = con.prepareStatement(sql1,Statement.RETURN_GENERATED_KEYS);
 			ps.setInt(1, comment.getFloorId());
 			ps.setInt(2, comment.getUserId());
 			ps.setString(3, comment.getContent());
 			ps.setInt(4, comment.getReplyId());
-			ps.setInt(5, comment.getShowName());
+			ps.setInt(5, comment.isShowName() ? 1 : 0);
+			ps.setInt(6, comment.isSheShowName() ? 1 : 0);
 			ps.execute();
 			con.commit();
 			
-			ResultSet rs = ps.getGeneratedKeys();
-			int result = 0;
+			rs = ps.getGeneratedKeys();
 			if (rs.next()) {
 				result = rs.getInt(1);
 			}
-			DBPool.close(con);
-			return result;
 		} catch (SQLException e) {
 			System.out.println("正在回滚");
 			try {
@@ -66,18 +60,28 @@ public class SubCommentsDao {
 				e1.printStackTrace();
 			}
 			e.printStackTrace();
-			return -1;
+			result = -1;
+		} finally {
+			DBPool.close(con);
 		}
+		return result;
 	}
 	
-	public static ArrayList<CommentDetail> selectCommentsByCount(boolean isMeeting,int id,int count){
-		ArrayList<CommentDetail> commentsList = new ArrayList<CommentDetail>();
+	public static ArrayList<SubComment> getSubCommentByFloorId(int type,int id,int count){
+		ArrayList<SubComment> commentsList = new ArrayList<>();
 		String sq0 = "use first_mysql_test";
-		String sql1;
-		if(isMeeting)
-			sql1 = "select a.*,b.showname,(b.vip > now()) as vip,(b.bigVip > now()) as bigVip from meetingComments a,user b where commentid = ? and a.userid = b.id order by bigVip desc,vip desc,date desc limit ?,?" ;
-		else
-			sql1 = "select a.*,b.showname,(b.vip > now()) as vip,(b.bigVip > now()) as bigVip from articleComments a,user b where commentid = ? and a.userid = b.id order by bigVip desc,vip desc,date desc limit ?,?" ;
+		String sql1 = null;
+		switch (type){
+			case 0://邀约
+				sql1 = "select a.*,b.name,(b.vip > now()) as vip,(b.bigVip > now()) as bigVip from meeting_sub_comments a,user b where floorid = ? and a.userid = b.id order by bigVip desc,vip desc,date desc limit ?,20" ;
+				break;
+			case 1:
+				sql1 = "select a.*,b.name,(b.vip > now()) as vip,(b.bigVip > now()) as bigVip from article_sub_comments a,user b where floorid = ? and a.userid = b.id order by bigVip desc,vip desc,date desc limit ?,20" ;
+				break;
+			case 2:
+				sql1 = "select a.*,b.name,(b.vip > now()) as vip,(b.bigVip > now()) as bigVip from circle_sub_comments a,user b where floorid = ? and a.userid = b.id order by bigVip desc,vip desc,date desc limit ?,20" ;
+				break;
+		}
 		Connection con = DBPool.getConnection();
 		PreparedStatement ps;
 		ResultSet rs;
@@ -87,27 +91,90 @@ public class SubCommentsDao {
 			ps = con.prepareStatement(sql1);
 			ps.setInt(1, id);
 			ps.setInt(2, count);
-			ps.setInt(3, 20);
 			rs = ps.executeQuery();
 			while (rs.next()) {
-				CommentDetail comment = new CommentDetail();
-				comment.setId(rs.getInt("id"));
-				comment.setCommentId(rs.getInt("commentid"));
+				SubComment comment = new SubComment();
+				comment.setFloorId(rs.getInt("floorid"));
 				comment.setUserId(rs.getInt("userid"));
-				comment.setCommentName(rs.getString("commentname"));
-				comment.setCommentGender(rs.getInt("commentgender"));
-				comment.setCommentContent(rs.getString("content"));
-				comment.setCommentDate(new Date(rs.getTimestamp("date").getTime()));
-				comment.setShow(rs.getInt("showname") > 0);
+				comment.setUserName(rs.getString("name"));
+				comment.setContent(rs.getString("content"));
+				comment.setDate(new Date(rs.getTimestamp("date").getTime()));
+				comment.setShowName(rs.getInt("showname") > 0);
+				comment.setSheShowName(rs.getInt("she_show_name") > 0);
 				comment.setIsVip(rs.getInt("vip") > 0);
 				comment.setBigVip(rs.getInt("bigVip") > 0);
 				commentsList.add(comment);
 			}
 		} catch (Exception e) {
-			System.out.println(e.getMessage().toString());
+			System.out.println(e.getMessage());
+		} finally {
 			DBPool.close(con);
 		}
-		DBPool.close(con);
 		return commentsList;
+	}
+
+	/**
+	 * 判断是否需要插入到前三条子评论的表
+	 * @param comment 评论内容
+	 * @param type 主题类型，对哪个进行评论，0-邀约;1-反馈;2-社区
+	 */
+	private static boolean needInsert2ThreeTable(SubComment comment, int type, Connection con) throws SQLException{
+		boolean result = false;
+		String sql1 = null;
+		switch (type){
+			case 0:
+				sql1 = "select count(*) as count from meeting_three_sub_comments where floorid = ?";
+				break;
+			case 1:
+				sql1 = "select count(*) as count from article_three_sub_comments where floorid = ?";
+				break;
+			case 2:
+				sql1 = "select count(*) as count from circle_three_sub_comments where floorid = ?";
+				break;
+		}
+		PreparedStatement ps;
+		ResultSet rs;
+		ps = con.prepareStatement(sql1);
+		ps.setInt(1, comment.getFloorId());
+		rs = ps.executeQuery();
+		while (rs.next()) {
+			result = rs.getInt("count") < 3;
+		}
+		return result;
+	}
+
+	/**
+	 * 插入到层主前三个评论中
+	 */
+	private static int insert2ThreeTable(SubComment comment, int type, Connection con) throws SQLException{
+		String sql1 = null;
+		switch(type){
+			case 0:
+				sql1 = "insert into meeting_three_sub_comments (floorid,userid,content,date,replyid,showname) values(?,?,?,now(),?,?)";
+				break;
+			case 1:
+				sql1 = "insert into article_three_sub_comments (floorid,userid,content,date,replyid,showname) values(?,?,?,now(),?,?)";
+				break;
+			case 2:
+				sql1 = "insert into circle_three_sub_comments (floorid,userid,content,date,replyid,showname) values(?,?,?,now(),?,?)";
+				break;
+		}
+		PreparedStatement ps;
+		ResultSet rs;
+		int result = 0;
+		con.setAutoCommit(false);
+		ps = con.prepareStatement(sql1);
+		ps.setInt(1, comment.getFloorId());
+		ps.setInt(2, comment.getUserId());
+		ps.setString(3, comment.getContent());
+		ps.setInt(4, comment.getReplyId());
+		ps.setInt(5, comment.isShowName() ? 1 : 0);
+		ps.execute();
+		con.commit();
+		rs = ps.getGeneratedKeys();
+		if (rs.next()) {
+			result = 1;
+		}
+		return result;
 	}
 }
